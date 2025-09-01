965b9f50cecfe54fb4cc25348a22a901c2016e97
Prince <prince00goswami@gmail.com>
2025-09-01 22:08:46 +0530

    Step 1- setup backend step2 - install required packages step 3 - create folders step 4 -- basic setup of server .js [ routes , middlewares,database ] step 5 -- setup dotenv and mongo uri  step 6 -- database and verify
     conclusion : Server running on port 8000
    MongoDB connected

---
+++ backend/server.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import path from 'path';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+
+dotenv.config();
+
+const app = express();
+
+// Connect to database
+connectDB();
+
+//Middlewares to handle CORS issues
+
+app.use(cors({
+  origin: process.env.CLIENT_URL || "*",
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+
+//Middlewares configuration
+
+app.use(express.json());
+
+// Routes
+// app.use("/api/auth", authRoutes);
+// app.use("/api/tasks", taskRoutes);
+// app.use("/api/users", userRoutes);
+// app.use("/api/report", reportRoutes);
+
+
+//server start
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
